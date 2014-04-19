4d716eb27925f329af1ce1714dd04ed9ec6d549c
Dmitri Vassilev <dmitrivassilev@gmail.com>
2014-04-18 20:48:45 -0400

    only push orders/carts if they have line items

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,1 +1,1 @@
-Spree::Order.after_commit -> { Spree::Hub::OrderSerializer.push_it(self) if Spree::Hub::Config[:enable_auto_push] }
+Spree::Order.after_commit -> { Spree::Hub::OrderSerializer.push_it(self) if Spree::Hub::Config[:enable_auto_push] && self.line_items.count > 0 }

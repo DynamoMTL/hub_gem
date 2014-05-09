d658ae61200e2c92435d64319355a4210c83b029
Sebastian Bean <sebastian@ginlanemedia.com>
2014-05-09 13:22:17 -0400

    found the culprit - moving to ref host app

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,17 +1,3 @@
 Spree::Order.send(:include, Spree::Hub::AutoPush)
 Spree::Order.hub_serializer = "Spree::Hub::OrderSerializer"
 Spree::Order.json_root_name = 'orders'
-
-Spree::Order.class_eval do
-  def push_to_hub
-    return unless self.line_items.count > 0
-    Spree::Hub::Client.push(serialized_payload)
-  end
-  def serialized_payload
-    ActiveModel::ArraySerializer.new(
-      [self],
-      each_serializer: self.class.hub_serializer.constantize,
-      root: order.completed_at.present? ? self.class.json_root_name : 'carts'
-    ).to_json
-  end
-end

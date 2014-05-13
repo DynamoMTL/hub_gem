dff62bafd2af71f788c55bd53444284fac15be5b
Sebastian Bean <sebastian@ginlanemedia.com>
2014-05-13 16:31:26 -0400

    disable shipment auto_push

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -1,3 +1,9 @@
 Spree::Shipment.send(:include, Spree::Hub::AutoPush)
 Spree::Shipment.hub_serializer = "Spree::Hub::ShipmentSerializer"
 Spree::Shipment.json_root_name = 'shipments'
+
+
+Spree::Shipment.class_eval do
+  def push_to_hub
+  end
+end
